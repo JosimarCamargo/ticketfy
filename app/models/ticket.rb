c3efd71f42fee2da4b1c4bbd97d6f6ff6fd91f63
Josimar Camargo <josimarcamargo@gmail.com>
2019-09-01 21:43:31 -0300

    Add title and content basic validation for ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Ticket < ApplicationRecord
   enum status: { without_status: 0, working: 1, solved: 2 }
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
