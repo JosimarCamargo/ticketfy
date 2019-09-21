77fd22bae590079eab987144f6acef0e59dd8768
Josimar Camargo <josimarcamargo@gmail.com>
2019-09-21 19:31:33 -0300

    add User model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+  # Devise added validations:
+  # email: presence, format(asd@asd)
+  # password: presence, size(min 6 digits)
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(user_params) }
+
+  describe 'validations' do
+    let(:user_params) { attributes_for(:user) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+
+    context 'when has email and password' do
+      let(:user_params) { attributes_for(:user) }
+      it { is_expected.to be_valid }
+    end
+
+    context 'when email has incorrect format' do
+      let(:user_params) { { password: '123456', email: '123' } }
+      it { is_expected.to be_invalid }
+    end
+  end
 end
