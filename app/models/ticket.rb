4890dd414e9b4df21abcbbf08ba3ed08cba98a3e
Josimar Camargo <josimar_camargo@hotmail.com>
2020-01-18 19:29:47 -0300

    TicketFilterService refactoring

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -10,4 +10,9 @@ class Ticket < ApplicationRecord
   validates :status, presence: true, inclusion: { in: statuses.keys }
   validates :title, presence: true
   validates :content, presence: true
+
+  scope :search_by_title, ->(title_param) { where('title ILIKE ?', title_param) if title_param }
+  scope :search_by_content, ->(content_param) { where('content ILIKE ?', content_param) if content_param }
+  scope :search_by_status, ->(status_param) { where(status: status_param) if status_param }
+  scope :search_by_id, ->(id_param) { where('tickets.id::TEXT LIKE ?', "%#{id_param}%") if id_param }
 end
--- app/services/ticket_filter_service.rb
+++ app/services/ticket_filter_service.rb
@@ -10,54 +10,29 @@ class TicketFilterService
   end
 
   def initialize(filter_params)
-    @query = Ticket
     @filter_params = OpenStruct.new(filter_params)
   end
 
   def call
-    @query = search_by_title
-    @query = search_by_content
-    @query = search_by_status
-    @query = search_by_id
-    @query = search_by_user_email
-    @query.all
+    query = Ticket.search_by_title(title)
+                   .search_by_content(content)
+                   .search_by_status(status)
+                   .search_by_id(id)
+    query = search_by_user_email(query)
+    query.all
   end
 
-  def search_by_user_email
+  def search_by_user_email(query)
     if search_by_user_assigned_email?
-      @query.joins(:user_assigned).where(users: { email: user_assigned_email })
+      query.joins(:user_assigned).where(users: { email: user_assigned_email })
     elsif search_by_requester_email?
-      @query.joins(:requester).where(users: { email: requester_email })
+      query.joins(:requester).where(users: { email: requester_email })
     elsif search_by_requester_email_or_search_by_user_assigned_email?
-      @query.joins(:requester).joins(:user_assigned)
+      query.joins(:requester).joins(:user_assigned)
             .where('users.email = ? OR users.email = ?', requester_email, user_assigned_email)
     else
-      @query
+      query
     end
-  end
-
-  def search_by_title
-    return @query if title.nil?
-
-    @query.where('title ILIKE ?', title)
-  end
-
-  def search_by_content
-    return @query if content.nil?
-
-    @query.where('content ILIKE ?', content)
-  end
-
-  def search_by_status
-    return @query if status.nil?
-
-    @query.where(status: status)
-  end
-
-  def search_by_id
-    return @query if id.nil?
-
-    @query.where('tickets.id::TEXT LIKE ?', "%#{id}%")
   end
 
   def search_by_user_assigned_email?
