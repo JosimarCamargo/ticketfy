9a570fc9fe5710e0c56409fce583ca8307950318
Josimar Camargo <josimar_camargo@hotmail.com>
2020-05-02 13:14:17 -0300

    Improves the Users Management Specs
    Fix problems to click on the right edit/delete button on the index
    
    Add the results expected to tests

--- spec/features/users_management_spec.rb
+++ spec/features/users_management_spec.rb
@@ -5,13 +5,17 @@ require 'rails_helper'
 RSpec.describe 'Users Management', type: :feature do
   let(:user) { create(:user) }
   feature 'User updates email and password' do
+    let(:new_email) { 'new@email.com' }
+    let(:new_password) { 'new_password' }
     scenario 'success' do
       sign_in(user)
       visit users_path
-      click_on 'Edit'
-      fill_in 'Email', with: 'new@email.com'
-      fill_in 'Password', with: 'new_password'
+      find("a[href='#{edit_user_path(user.id)}']").click # Edit Button
+      fill_in 'Email', with: new_email
+      fill_in 'Password', with: new_password
       click_on 'Update User'
+      expect(user.reload.email).to eq(new_email)
+      expect(user.valid_password?(new_password)).to be true
       expect(page).to have_content('You need to sign in or sign up before continuing.')
     end
   end
@@ -20,13 +24,14 @@ RSpec.describe 'Users Management', type: :feature do
     scenario 'success' do
       sign_in(user)
       visit users_path
-      click_on 'Destroy'
+      find("a[href='#{user_path(user.id)}'][data-method='delete']").click # Delete Button
       page.driver.browser.switch_to.alert.accept
       expect(page).to have_content('You need to sign in or sign up before continuing.')
+      expect(User.find_by(id: user.id)).to be nil
     end
   end
 
-  feature 'Users are listed and paginate' do
+  feature 'Users are listed and paginated' do
     let!(:user_2) { create_list(:user, 21).first }
     scenario 'success' do
       sign_in(user)
